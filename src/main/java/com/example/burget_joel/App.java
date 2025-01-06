1aea147611c20df95dd6b9a9467762ffcd153178
Joel <134548469+Joel-Burget@users.noreply.github.com>
2025-01-06 12:50:02 -0700

    adding code to move an object with the arrow keys

--- src/main/java/com/example/burget_joel/App.java
+++ src/main/java/com/example/burget_joel/App.java
@@ -5,10 +5,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -21,21 +23,45 @@ import java.io.IOException;
 public class App extends Application {
 
     private static Scene scene;
+    private Rectangle shape;
+    private double speed = 5;
 
     @Override
     public void start(Stage stage) throws IOException {
         GridPane pane = new GridPane();
 
-        Circle circle = new Circle(100);
-        circle.setStroke(Color.RED);
-        circle.setFill(Color.GREEN);
-        pane.add(circle, 0, 0);
+        shape = new Rectangle(100, 50, 100, 50);
+        shape.setStroke(Color.RED);
+        shape.setFill(Color.GREEN);
+        pane.add(shape, 0, 0);
         
         
-        Scene scene = new Scene(pane, 250, 250);
+        Scene scene = new Scene(pane, 1250, 750);
+        scene.setOnKeyPressed(this::handleKeyPressed);
+
         stage.setTitle("Untitled Corgi Game");
         stage.setScene(scene);
         stage.show();
+    }
+
+    private void handleKeyPressed(KeyEvent event){
+        switch (event.getCode()) {
+            case UP:
+                shape.setY(shape.getY() - speed);
+                break;
+            case DOWN:
+                shape.setY(shape.getY() + speed);
+                break;
+
+            case LEFT:
+                shape.setX(shape.getX() - speed);
+                break;
+            case RIGHT:
+                shape.setX(shape.getX() + speed);
+            default:
+                break;
+        }
+        System.out.println(event.getCode());
     }
 
     static void setRoot(String fxml) throws IOException {
